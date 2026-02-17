1a679125db6ca35e64cd7c35ef2ea1ecc59cfc23
Eric Seidel <ewjseidel@gmail.com>
2026-02-17 14:52:23 -0500

    testing preview link

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
     <main style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
       <h1>Heirloom API</h1>
       <p>Your local Next.js app is live! 🚀</p>
-      <p>this is a test preview.</p>
+      <p>this is a test preview of the preview link job.</p>
     </main>
   )
 }
